75fd538c4ac43d42d8456476a440a48e8f53298e
tjgaither <78992877+tjgaither@users.noreply.github.com>
2021-02-12 23:31:19 -0500

    Added Files

---
+++ script.js
@@ -0,0 +1,3 @@
+var name= prompt("What is your name?");
+var color= prompt("What is your favorite color?");
+document.body.querySelector(".writer").innerHTML=name+" is my name! "+color+" is my favorite color!";
